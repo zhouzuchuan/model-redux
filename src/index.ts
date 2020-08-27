5ecd643c345220a894363c1053bfa7faaa5361d2
fezzc <zhouzuchuan@sina.com>
2020-08-27 14:12:28 +0800

    :bulb: 修改类型标注

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,37 @@
-import configureStore from './configureStore';
-import registerModel from './registerModel';
-import { MODELS, STORE } from './config';
-import { isFunction, isArray, isObject } from './utils';
-import { REDUCERS } from './config';
+import configureStore from "./configureStore";
+import registerModel from "./registerModel";
+import { MODELS, STORE } from "./config";
+import { isFunction, isArray } from "./utils";
+import { REDUCERS } from "./config";
 
 export interface ModelConfig {
     middlewares?: never[];
     effects?: null | any;
-    persist?: any;
-    initialReducers: any;
+    initialReducers?: any;
 }
 
-export const create = ({ middlewares = [], effects = null, persist, initialReducers = {} } = {} as ModelConfig) => {
+export const create = (
+    {
+        middlewares = [],
+        effects = null,
+        initialReducers = {},
+    } = {} as ModelConfig
+) => {
     const app = {
         [MODELS]: [],
         effectsList:
             effects === null
                 ? // 取默认
-                  require('./effects/epics').default()
-                : (isArray(effects) ? effects : [effects]).reduce((r: any, fn: () => any) => {
-                      return {
-                          ...r,
-                          ...(isFunction(fn) ? fn() : fn),
-                      };
-                  }, {}),
+                  require("./effects/epics").default()
+                : (isArray(effects) ? effects : [effects]).reduce(
+                      (r: any, fn: () => any) => {
+                          return {
+                              ...r,
+                              ...(isFunction(fn) ? fn() : fn),
+                          };
+                      },
+                      {}
+                  ),
         [STORE]: null,
         [REDUCERS]: initialReducers,
     };
